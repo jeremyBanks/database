19169adb1e6724ccec3b76db6994b9965739e7ce
Jeremy Banks <_@jeremy.ca>
2021-02-18 16:35:51 -0500

    00000024-0fa5190f

--- sql/sql_test.ts
+++ sql/sql_test.ts
@@ -2,6 +2,8 @@ import * as driver from "./_dummy_driver.ts";
 
 import * as sql from "./sql.ts";
 
-Deno.test("testing with dummy_driver", async () => {
+Deno.test("dumbing with with dummy_driver", async () => {
   const db = await sql.open(":memory:", driver);
+
+  db.query("hello");
 });
